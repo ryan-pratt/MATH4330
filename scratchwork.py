508aba79141c708e757baa10c08420c1d9c797b2
ryan-pratt <thechanceofrain@gmail.com>
2019-04-30 12:50:01 -0500

    formatting (line length)

--- scratchwork.py
+++ scratchwork.py
@@ -39,8 +39,9 @@ def add_vectors(vector_a, vector_b):
 def subtract_vectors(vector_a, vector_b):
     """Subtracts two vectors of equal dimensions
 
-    This takes two vectors, as lists, of equal length, computes their difference,
-    and returns that difference as a list by adding the negation of vector_b to vector_a.
+    This takes two vectors, as lists, of equal length, computes their
+    difference, and returns that difference as a list by adding the negation 
+    of vector_b to vector_a.
 
     Args:
         vector_a (list): first vector
@@ -96,11 +97,13 @@ def conjugate_matrix(matrix):
 def orthogonal_decomposition(orthonormal_set, vector):
     """Computes an orthogonal vector
 
-    This computes the orthogonal decomposition of a vector with respect to orthonormal_set
+    This computes the orthogonal decomposition of a vector with respect 
+    to orthonormal_set
 
     Args:
         orthonormal_set (list of lists): orthonormal list of vectors
-        vector (list): a vector of compatible dimension to the vectors in orthonormal_set
+        vector (list): a vector of compatible dimension to the vectors 
+        in orthonormal_set
 
     Returns:
         list: a vector that is orthogonal to the vectors in orthonormal_set
@@ -179,8 +182,9 @@ def multiply_matrix_vector(matrix, vector):
 def multiply_matrices(matrix_a, matrix_b):
     """Multiplies two matrices of compatible dimensions
 
-    This computes the multiplication of two matrices by computing the dot product
-    of each column of the first matrix with each column of the second matric.
+    This computes the multiplication of two matrices by computing the 
+    dot product of each column of the first matrix with each column of 
+    the second matric.
 
     Args:
         matrix_a (list of lists): first matrix as list of columns
