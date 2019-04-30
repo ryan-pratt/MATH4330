c0e3130c98552c2ec8a196a849a1e509be7d9ed2
ryan-pratt <thechanceofrain@gmail.com>
2019-04-30 13:14:02 -0500

    scaffold project code

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+def vandermonde(n, matrix):
+    #TODO
+
+def modified_gs(matrix):
+    #TODO
+
+def conjugate_transpose(matrix):
+    #TODO
+
+def back_substitution(R, b):
+    #TODO
+
+def matrix_vector_mult(matrix, vector):
+    #TODO
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import algorithms.py as algs
 
 if __name__ == "__main__":
-    algorithms.p_norm(2, vector_a)
+    x = some vector
+    y = some vector
+
+    A = algs.vandermonde(4, x)
+    gs = algs.modified_gs(A)
+    Q = gs[0]
+    R = gs[1]
+    Q_star = algs.conjugate_transpose(Q)
+    alpha = algs.back_substitution(R, algs.matrix_vector_mult(Q_star, y))
+    print(f'p(x) = {alpha[0]} + {alpha[1]}x + {alpha[2]}x^2 + {alpha[3]}x^3')
