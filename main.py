16291c13a1e6886ff5971944716876ed24176e33
ryan-pratt <thechanceofrain@gmail.com>
2019-04-30 13:26:40 -0500

    restructure main

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import algorithms.py as algs
 
-if __name__ == "__main__":
-    x = some vector
-    y = some vector
-
-    A = algs.vandermonde(4, x)
+def degree_4_interpolation(features, labels):
+    A = algs.vandermonde(4, features)
     gs = algs.modified_gs(A)
     Q = gs[0]
     R = gs[1]
     Q_star = algs.conjugate_transpose(Q)
-    alpha = algs.back_substitution(R, algs.matrix_vector_mult(Q_star, y))
+    alpha = algs.back_substitution(R, algs.matrix_vector_mult(Q_star, labels))
     print(f'p(x) = {alpha[0]} + {alpha[1]}x + {alpha[2]}x^2 + {alpha[3]}x^3')
