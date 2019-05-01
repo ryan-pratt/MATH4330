e6dae40af8460c18645e5c197ff9ab1bd2b5aced
ryan-pratt <thechanceofrain@gmail.com>
2019-04-30 20:59:32 -0500

    add test case

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import algorithms.py as algs
+import algorithms as algs
 
 def degree_4_interpolation(features, labels):
     A = algs.vandermonde(4, features)
@@ -7,4 +7,9 @@ def degree_4_interpolation(features, labels):
     R = gs[1]
     Q_star = algs.conjugate_transpose(Q)
     alpha = algs.back_substitution(R, algs.matrix_vector_mult(Q_star, labels))
-    print(f'p(x) = {alpha[0]} + {alpha[1]}x + {alpha[2]}x^2 + {alpha[3]}x^3')
+    print("p(x) = {0} + {1}x + {2}x^2 + {3}x^3".format(alpha[0], alpha[1], alpha[2], alpha[3]))
+
+if __name__ == "__main__":
+    f = [1+0j, 2+0j, 3+0j, 4+0j]
+    l = [3+0j, 2+0j, 1+0j, 0+0j]
+    degree_4_interpolation(f, l)
