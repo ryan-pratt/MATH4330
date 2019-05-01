dfe0db3ae56c52a72f88653b2ffaee583b445bc6
ryan-pratt <thechanceofrain@gmail.com>
2019-04-30 21:00:29 -0500

    fix various bugs

--- algorithms.py
+++ algorithms.py
@@ -1,24 +1,24 @@
 def vandermonde(n, vector):
     result = [[0] * len(vector)] * n
-    for i in range(n-1):
-        col = []
-        for j in range(1, m):
+    for i in range(n):
+        col = [0] * len(vector)
+        for j in range(len(vector)):
             col[j] = vector[j] ** i
-        result[i+1] = col
+        result[i] = col
     return result
 
 def modified_gs(matrix):
     temp = [[0] * len(matrix[0])] * len(matrix)
     R = [[0] * len(matrix[0])] * len(matrix)
     Q = [[0] * len(matrix[0])] * len(matrix)
-    for i in range(n):
+    for i in range(len(matrix)):
         temp[i] = matrix[i]
-    for i in range(n):
+    for i in range(len(matrix)):
         R[i][i] = p_norm(2, temp[i])
         Q[i] = scalar_vector_mult(inverse(R[i][i]), temp[i])
-        for j in range(i+1, n):
+        for j in range(i+1, len(matrix)):
             R[i][j] = dot_product(Q[i], temp[j])
-            temp[j] = vector_subtract(v[j], scalar_vector_mult(R[i][j], Q[i]))
+            temp[j] = vector_subtract(temp[j], scalar_vector_mult(R[i][j], Q[i]))
     return [Q, R]
 
 def p_norm(p, v):
@@ -42,8 +42,8 @@ def conjugate_transpose(matrix):
 
 def back_substitution(R, b):
     result = b
-    for i in range(n, 1, -1):
-        for j in range(i+1, n):
+    for i in range(len(b)-1, 1, -1):
+        for j in range(i+1, len(b)):
             result[i] -= R[i][j] * result[j]
         result[i] *= inverse(R[i][i])
     return result
@@ -54,17 +54,17 @@ def inverse(s):
 def matrix_vector_mult(matrix, vector):
     result = [0] * len(vector)
     for i in range(len(matrix)):
-        result[i] = vector_dot_product(matrix[i], vector)
+        result[i] = dot_product(matrix[i], vector)
     return result
 
-def vector_dot_product(a, b):
+def dot_product(a, b):
     result = 0;
-    for i in range(n):
+    for i in range(len(a)):
         result += conjugate(a[i]) * b[i]
     return result
 
 def conjugate(s):
-    return s.real - s.imaj
+    return s.real - s.imag
 
 def conjugate_matrix(matrix):
     for col in matrix:
